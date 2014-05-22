6bdbb5eff0096e7571968e11d340730c1da5e268
Mike Boers <github@mikeboers.com>
2014-05-21 17:25:51 -0700

    Use mi_instance_lightlist

--- KSHair.cpp
+++ KSHair.cpp
@@ -1,38 +1,5 @@
-// ==========================================================================
-// Copyright 2008 Autodesk, Inc. All rights reserved.
-//
-// Use of this software is subject to the terms of the Autodesk
-// license agreement provided at the time of installation or download,
-// or which otherwise accompanies this software in either electronic
-// or hard copy form.
-// ==========================================================================
-
-/*****************************************************************************
- * This example takes the mental ray maya_illum_phong devkit example and 
- * adds support for a subset of the Maya render passes. This differs from the
- * MayaPhong example in that here we define the light loop ourselves, while in
- * MayaPhong the AdskShaderSDK light loop is used and we only implement the
- * specular shading component.
- *
- * Because we write our own light loop, we are also responsible for
- * contributing to the appropriate render passes. See the "RENDER PASS
- * SPECIFIC" sections below. We contribute to ambient material color, diffuse
- * material color, diffuse, direct irradiance, specular, indirect and beauty
- * passes. Support for other passes could easily be added by computing the
- * correct value and contributing in the same manner. See adskShader.h for
- * more details on the other passes.
- *
- * This shader does not support per-light contributions defined by pass
- * contribution maps, however it does support per-object contributions as long
- * as the shader's shading group is exported with the Maya Shading Engine. For
- * details on supporting per-light contributions see adskShader.h
- *****************************************************************************/
-
 #include "adskShader.h"
 
-// Old headers.
-// #include "shader.h"
-// #include "mi_shader_if.h"
 
 struct KSHairParameters {
 	ADSK_BASE_SHADER_PARAMETERS
@@ -42,10 +9,8 @@ struct KSHairParameters {
 	miColor     specular;
 	miScalar    exponent;
 	miInteger   mode;
-	int		    i_light;
-	int		    n_light;
-	miTag		light[1];
 };
+
 
 const unsigned int KSHAIR_VERSION = 1;
 typedef ShaderHelper<KSHairParameters> BaseShaderHelperType;
@@ -64,7 +29,7 @@ public:
 		{}
 	~KSHairClass() {}
 
-	miBoolean operator()(miColor *pResult, miState *state, KSHairParameters *params);
+	miBoolean operator()(miColor *result, miState *state, KSHairParameters *params);
 
 private:
 
@@ -161,19 +126,8 @@ miBoolean KSHairClass::operator()(miColor *result, miState *state, KSHairParamet
 	exponent   = *mi_eval_scalar(&paras->exponent);
 	mode       = *mi_eval_integer(&paras->mode);
 
-	i_light    = *mi_eval_integer(&paras->i_light);
-	n_light    = *mi_eval_integer(&paras->n_light);
-	light      =  mi_eval_tag(paras->light) + i_light;
-
-	/* correct light list, if requested */
-	if (mode == 1)
-		mi_inclusive_lightlist(&n_light, &light, state);
-	else if (mode == 2)
-	 	mi_exclusive_lightlist(&n_light, &light, state);
-	else if (mode == 4) {
-	 	n_light = 0;
-	 	light = 0;
-	}
+	// Get the light list.
+	mi_instance_lightlist(&n_light, &light, state);
 
 	/* modify diffuse colour to give darker colour near
 	   root. this may obviate the need for real shadows,
@@ -276,160 +230,3 @@ miBoolean KSHairClass::operator()(miColor *result, miState *state, KSHairParamet
 
 EXPOSE(KSHair, miColor, );
 
-
-/*
-
-miBoolean KSHairClass::operator()(miColor *pResult, miState *state, KSHairParameters *params)
-{
-	// Parameters.
-	miColor *ambi, *diff, *spec;
-	miScalar expo;
-	miInteger mode;
-
-	// Lights.
-	miTag *lights;
-	int numLights;
-    LightDataArray *inLightData;			 
-
-
-	// Check for illegal calls.
-	if (state->type == miRAY_SHADOW || state->type == miRAY_DISPLACE ) {
-		return miFALSE;
-	}
- 
- 	// Evaluate parameters.
-	ambi =  mi_eval_color(&params->ambient);
-	diff =  mi_eval_color(&params->diffuse);
-	spec =  mi_eval_color(&params->specular);
-	expo = *mi_eval_scalar(&params->exponent);
-	mode = *mi_eval_integer(*params->mode);
-
-	*pResult    = *mi_eval_color(&params->ambience); // ambient term
-	pResult->r *= ambi->r;
-	pResult->g *= ambi->g;
-	pResult->b *= ambi->b;
-
-	// RENDER PASS SPECIFIC
-	//   Write values to non-light dependant frame buffers
-    if (numberOfFrameBuffers && MaterialBase::mFrameBufferWriteOperation)
-    {
-		MaterialBase::writeToFrameBuffers(state, frameBufferInfo, passTypeInfo, opaqueColor(*ambi), AMBIENT_MATERIAL_COLOR, false);
-        MaterialBase::writeToFrameBuffers(state, frameBufferInfo, passTypeInfo, opaqueColor(*diff), DIFFUSE_MATERIAL_COLOR, false);
-    }
-
-	inLightData = &MBS->lightData;
-    miColor  *preShadowColor = &inLightData->preShadowColor;
-
-	// Get instance light list. *
-    mi_instance_lightlist(&numLights, &lights, state);
-    // Evaluate all lights in the list. *
-    for ( ; numLights--; lights++)         
-	{
-		int    numSamples = 0;
-		diffSum.r = diffSum.g = diffSum.b = 0;
-		specSum.r = specSum.g = specSum.b = 0;
-
-		// Function that initialize light sample accumulators which need to be called before the sample loop.
-		sampleLightBegin(numberOfFrameBuffers, frameBufferInfo);
-
-		while (mi_sample_light(
-                &color, &dir, &dot_nl,
-                state, *lights, &numSamples)) {
-	
-			// Call to enable renderpass contributions for light shaders that were not developped using the AdskShaderSDK.    
-			handleNonAdskLights(numberOfFrameBuffers, frameBufferInfo, color, *lights, state);
-			
-			LightDataArray *ldat = &MBS->lightData;
-			if (ldat){
-			    emitDiffuse = ldat->lightDiffuse;
-			    emitSpecular = ldat->lightSpecular;
-			}
-
-			// Add diffuse component
-			// only if the light emits diffuse lights
-			miColor unlitDiffuse = BLACK;
-			if (emitDiffuse && dot_nl > 0) {
-				unlitDiffuse = opaqueColor(dot_nl*(*diff));
-				// Lambert's cosine law
-				if (numberOfFrameBuffers && MaterialBase::mFrameBufferWriteOperation)
-				{
-					MaterialBase::writeToFrameBuffers(state, frameBufferInfo, passTypeInfo, unlitDiffuse, DIFFUSE, true);
-					MaterialBase::writeToFrameBuffers(state, frameBufferInfo, passTypeInfo, opaqueColor(dot_nl*WHITE), DIRECT_IRRADIANCE, true);
-				}
-
-				//Compute values for the master beauty pass
-				diffSum = diffSum + unlitDiffuse * color;
-			}
-
-			// Add specular component
-			// only if the light emits specular lights
-			miColor unlitSpecular = BLACK;
-			if (emitSpecular) {
-				// Phong's cosine power
-				s = mi_phong_specular(expo, state, &dir);
-				if (s > 0.0) {
-					unlitSpecular = opaqueColor(s*(*spec));
-					if (numberOfFrameBuffers && MaterialBase::mFrameBufferWriteOperation)
-					{
-						MaterialBase::writeToFrameBuffers(state, frameBufferInfo, passTypeInfo, unlitSpecular, SPECULAR, true);
-					}
-					specSum = specSum + unlitSpecular * color;
-				}
-			}
-
-			miColor unlitBeauty = opaqueColor(unlitSpecular+unlitDiffuse);
-
-            MaterialBase::writeToFrameBuffers(state, frameBufferInfo, passTypeInfo, 
-                unlitBeauty, SHADOW, true, preShadowColor);
-            MaterialBase::writeToFrameBuffers(state, frameBufferInfo, passTypeInfo, 
-                MAYA_LUMINANCE(unlitBeauty), SHADOW_MONO, true, preShadowColor);
-            MaterialBase::writeToFrameBuffers(state, frameBufferInfo, passTypeInfo, 
-                unlitBeauty, BEAUTY, true );
-		}
-
-		// Function that take care of combining sample values into the material frame buffer values, 
-		//    called after light sampling loop.
-		sampleLightEnd(state,
-			   numberOfFrameBuffers,
-			   frameBufferInfo,
-			   numSamples,
-			   MaterialBase::mFrameBufferWriteOperation,
-			   MaterialBase::mFrameBufferWriteFlags,
-			   MaterialBase::mFrameBufferWriteFactor);
-
-		if (numSamples > 0) {
-			inv = 1.0f / numSamples;
-
-			diffSum.r *= inv;
-			diffSum.g *= inv;
-			diffSum.b *= inv;
-
-			specSum.r *= inv;
-			specSum.g *= inv;
-			specSum.b *= inv;
-
-			pResult->r += diffSum.r + specSum.r;
-			pResult->g += diffSum.g + specSum.g;
-			pResult->b += diffSum.b + specSum.b;
-		}
-	} // for ( ; numLights--; lights++) *
-
-	// add contribution from indirect illumination (caustics)
-	mi_compute_irradiance(&color, state);
-	pResult->r += color.r * diff->r;
-	pResult->g += color.g * diff->g;
-	pResult->b += color.b * diff->b;
-	pResult->a  = 1;
-
-	// RENDER PASS SPECIFIC
-	//   Write indirect illumination and final beauty to frame buffers
-    if (numberOfFrameBuffers && MaterialBase::mFrameBufferWriteOperation)
-    {
-		MaterialBase::writeToFrameBuffers(state, frameBufferInfo, passTypeInfo, opaqueColor(color), INDIRECT, false);
-		MaterialBase::writeToFrameBuffers(state, frameBufferInfo, passTypeInfo, opaqueColor(color*(*diff)), BEAUTY, false);
-    }
-
-	return(miTRUE);
-}
-
-*/
